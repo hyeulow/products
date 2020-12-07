942b128b5585b9ec3511ad464c4fbb308029af2e
haoyeu <bobbycookie213@gmail.com>
2020-12-07 18:01:58 +0800

    add for loop in printing products

--- products.py
+++ products.py
@@ -11,3 +11,8 @@ while True:
 	#更快速的清单写法是 products.append([p_n, p_p])
 print(products)
 print(products[0][0])
+
+
+#for loop搞清楚每个东西,在 products 里的每一个p 是大清单里的小清单 [['rice', '1.5'], ['meat', '2.5']] ，['rice', '1.5'] 为一个p
+for p in products:
+	print(p[0])
